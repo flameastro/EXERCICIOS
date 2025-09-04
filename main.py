2ec4ba907490d837a2082f1f5d1fbe2eb1abf0c5
flameastro <gabrielgoulartbnu@gmail.com>
2025-09-04 17:09:47 -0300

    new exercises

--- main.py
+++ main.py
@@ -3020,3 +3020,80 @@ class Gato:
 if __name__ == "__main__":
     patrick = Gato("Patrick", "Cinza Claro")
     patrick.miar()
+
+
+# ex159: Crie uma função que receba quantidade como parâmetro e retorne n lorems, em que n é a quantidade.
+from random import choice
+
+
+def lorem(quantidade: int):
+    lorems = [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.",
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    ]
+
+    for item in range(quantidade):
+        print(choice(lorems))
+
+
+if __name__ == "__main__":
+    lorem(5)
+    lorem(12)
+    lorem(9)
+
+
+# ex160: Classe Academia: parâmetros -> nome, endereco, equipamentos; Funções -> abrir, fechar
+class Academia:
+    def __init__(self, nome, endereco, equipamentos):
+        self.nome = nome
+        self.endereco = endereco
+        self.equipamentos = equipamentos
+        self.aberto = False
+
+    def sobre(self):
+        print(f"Seja bem vindo(a) ao {self.nome} localizado em {self.endereco}! Aqui temos equipamentos como {", ".join(self.equipamentos)}!")
+        if self.aberto:
+            print("Neste momento estamos abertos, esperando por você!")
+        else:
+            print("A academia está fechada agora. Volte mais tarde, esperamos por você!")
+
+    def usar_academia(self, equipamento):
+        if self.aberto:
+            print(f"Bora crescer com tudo com {equipamento}!")
+        else:
+            print("Não é possível usar a academia quando está fechada!")
+
+    def abrir(self):
+        if self.aberto:
+            print("A academia já está aberta.")
+        else:
+            self.aberto = True
+            print("Academia abriu, venha nos visitar!")
+
+    def fechar(self):
+        if self.aberto:
+            print("Estamos fechando, volte amanhã mais cedo.")
+            self.aberto = False
+        else:
+            print("A academia já está fechada.")
+
+    def status(self):
+        if self.aberto:
+            print("A academia está aberta.")
+        else:
+            print("A academia está fechada.")
+
+
+if __name__ == "__main__":
+    muscle_grow = Academia("MuscleGrow", "Rua Fransisco Ladelmir 778", ["...", "...", "..."])
+    muscle_grow.status()
+    muscle_grow.sobre()
+    muscle_grow.abrir()
+    muscle_grow.usar_academia("Supino")
+    muscle_grow.fechar()
